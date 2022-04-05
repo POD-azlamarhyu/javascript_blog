d25af37f25d60c526d51457eb838738a890b0e87
jikobouei-capital <adrague.ggoch@gmail.com>
2022-04-05 18:01:22 +0900

    secondry

--- js44/dist/script.dev.js
+++ js44/dist/script.dev.js
@@ -2,27 +2,37 @@
 
 var doc = document;
 var contry = doc.getElementById("js-select");
-var checkbox = doc.querySelectorAll("input");
+var checkboxes = doc.getElementsByClassName("cb");
+var radioes = doc.getElementsByClassName("radio");
+var msg = doc.getElementById("js-msg");
+var bg = doc.getElementById("js-msgbg");
 
 var onChangeEvent1 = function onChangeEvent1() {
   console.log("".concat(contry.value));
 };
 
-contry.addEventListener("change", onChangeEvent1);
+var onChangeEvent2 = function onChangeEvent2() {
+  console.log(" ------- ");
+
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      console.log(checkboxes[i].value);
+    }
+  }
+
+  console.log(" ------- ");
+};
+
+contry.addEventListener("change", onChangeEvent1); // js_cb1.addEventListener("change",onChangeEvent2);
+
 var _iteratorNormalCompletion = true;
 var _didIteratorError = false;
 var _iteratorError = undefined;
 
 try {
-  var _loop = function _loop() {
+  for (var _iterator = checkboxes[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
     var cb = _step.value;
-    cb.addEventListener("change", function () {
-      console.log("".concat(cb.value));
-    });
-  };
-
-  for (var _iterator = checkbox[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-    _loop();
+    cb.addEventListener("change", onChangeEvent2);
   }
 } catch (err) {
   _didIteratorError = true;
@@ -35,6 +45,58 @@ try {
   } finally {
     if (_didIteratorError) {
       throw _iteratorError;
+    }
+  }
+}
+
+var onChangeEvent3 = function onChangeEvent3() {
+  var checkValue;
+
+  for (var i = 0; i < radioes.length; i++) {
+    if (radioes[i].checked) {
+      checkValue = radioes[i].value;
+    }
+  }
+
+  msg.innerHTML = checkValue;
+
+  if (checkValue === "株式") {
+    msg.style.backgroundColor = "#99ffff";
+  } else if (checkValue === "債権") {
+    msg.style.backgroundColor = "#ff9999";
+  } else if (checkValue === "不動産") {
+    msg.style.backgroundColor = "#66cccc";
+  } else if (checkValue === "投資信託") {
+    msg.style.backgroundColor = "#ffcc66";
+  } else if (checkValue === "金") {
+    msg.style.backgroundColor = "#cccc00";
+  } else if (checkValue === "外貨預金") {
+    msg.style.backgroundColor = "#33ff33";
+  } else {
+    msg.style.backgroundColor = "#ff99ff";
+  }
+};
+
+var _iteratorNormalCompletion2 = true;
+var _didIteratorError2 = false;
+var _iteratorError2 = undefined;
+
+try {
+  for (var _iterator2 = radioes[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+    var rd = _step2.value;
+    rd.addEventListener("change", onChangeEvent3);
+  }
+} catch (err) {
+  _didIteratorError2 = true;
+  _iteratorError2 = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion2 && _iterator2["return"] != null) {
+      _iterator2["return"]();
+    }
+  } finally {
+    if (_didIteratorError2) {
+      throw _iteratorError2;
     }
   }
 }
--- js44/script.js
+++ js44/script.js
@@ -1,18 +1,60 @@
 const doc = document;
 const contry = doc.getElementById("js-select");
-const checkbox = doc.querySelectorAll("input");
+const checkboxes = doc.getElementsByClassName("cb");
+const radioes = doc.getElementsByClassName("radio");
+const msg = doc.getElementById("js-msg");
+const bg = doc.getElementById("js-msgbg");
 
 const onChangeEvent1 = () => {
     console.log(`${contry.value}`);
 }
 
 
+const onChangeEvent2 = () =>{
+    console.log(" ------- ");
+    for(let i = 0;i < checkboxes.length;i++){
+        if(checkboxes[i].checked){
+            console.log(checkboxes[i].value);
+        }
+    }
+    console.log(" ------- ");
+}
 
 
 contry.addEventListener("change",onChangeEvent1);
 
-for(let cb of checkbox){
-    cb.addEventListener("change",() => {
-        console.log(`${cb.value}`);
-    });
+// js_cb1.addEventListener("change",onChangeEvent2);
+
+for(let cb of checkboxes){
+    cb.addEventListener("change",onChangeEvent2);
+}
+
+const onChangeEvent3 = () =>{
+    let checkValue;
+    for(let i = 0;i < radioes.length;i++){
+        if(radioes[i].checked){
+            checkValue = radioes[i].value;
+        }
+    }
+
+    msg.innerHTML = checkValue;
+    if(checkValue === "株式"){
+        msg.style.backgroundColor = "#99ffff";
+    }else if(checkValue === "債権"){
+        msg.style.backgroundColor = "#ff9999";
+    }else if(checkValue === "不動産"){
+        msg.style.backgroundColor = "#66cccc";
+    }else if(checkValue === "投資信託"){
+        msg.style.backgroundColor = "#ffcc66";
+    }else if(checkValue === "金"){
+        msg.style.backgroundColor = "#cccc00";
+    }else if(checkValue === "外貨預金"){
+        msg.style.backgroundColor = "#33ff33";
+    }else{
+        msg.style.backgroundColor = "#ff99ff";
+    }
+}
+
+for(let rd of radioes){
+    rd.addEventListener("change",onChangeEvent3);
 }
